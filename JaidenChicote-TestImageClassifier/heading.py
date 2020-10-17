68584fe9f54dbe270e73a178cba04a938e36717e
Jaiden Chicote <jedezzio@gmail.com>
2020-10-17 15:02:52 +1100

    Added files for test image classifiar using CIFAR10 image datasets

---
+++ JaidenChicote-TestImageClassifier/heading.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+
